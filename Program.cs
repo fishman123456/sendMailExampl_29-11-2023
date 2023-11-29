7031aa502521ba6c071e23ece8b4f9a781b14605
fishman123456 <den9152@yandex.ru>
2023-11-30 00:34:38 +0700

    отправил сообщение на ящик mail.ru

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
-﻿namespace sendMailExampl_29_11_2023
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace sendMailExampl_29_11_2023
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SmtpClient smtpClient = new SmtpClient("smtp.mail.ru");
+            smtpClient.Port = 587;
+            smtpClient.EnableSsl = true;
+            smtpClient.Credentials = new NetworkCredential ("A_Fiscman@list.ru", "ittEHuHPF0TDK19ZqAT2");
+            // отправка сообщения
+            smtpClient.Send ("A_Fiscman@list.ru", "den9152@yandex.ru", "twrthrt", "Здрасти");
+            Console.WriteLine("сообщение отправлено");
         }
     }
 }
