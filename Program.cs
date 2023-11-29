fe7caa5ef2f4ef48f2b49ceda66f0edee90ca2ce
fishman123456 <den9152@yandex.ru>
2023-11-29 23:45:34 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace sendMailExampl_29_11_2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
